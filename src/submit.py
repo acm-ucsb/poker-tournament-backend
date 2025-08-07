4f92445169b8c0905899e0a060f9ffd141e0603a
Daniel Cui <solunian@gmail.com>
2025-08-07 14:09:20 -0700

    feat: mutex on fs, pathlib, clean up

--- src/submit.py
+++ src/submit.py
@@ -1,95 +1,101 @@
 import os
+import subprocess
+import pathlib
 
-# import subprocess
 from fastapi import APIRouter, Depends, HTTPException, UploadFile, status
 from gotrue import User
 from src.util.auth import verify_user
 from src.util.models import auth_required_responses
 from src.util.supabase_client import db_client
+import asyncio
 
 submit_router = APIRouter(prefix="/submit", tags=["submit"])
 
+# all files are sitting in this dir, <team_id>.<cpp | py>
+uploads_dir = pathlib.Path("..", "poker_tournament_uploads").resolve()
 
-@submit_router.post("/file/", response_model=dict, responses=auth_required_responses)
+# actually using a mutex, for any fs writes, preventing race conditions
+file_lock = asyncio.Lock()
+
+
+# if re-upload a file, old file will get deleted!
+@submit_router.post("/", responses=auth_required_responses)
 async def submit_file(file: UploadFile, user: User = Depends(verify_user)):
-    try:
-        content = await file.read()
+    # decode bytes -> str
+    # add .replace("\r\n", "\n") if want to normalize line ending wtvr
+    content_str = (await file.read()).decode("utf-8")
 
-        # decode bytes -> str, normalize line endings from windows to unix (eh doesn't matter)
-        content_str = content.decode("utf-8")  # .replace("\r\n", "\n")
+    # get team id from foreign key in users table
+    team_id = (
+        db_client.table("users").select("team_id").eq("id", user.id).single().execute()
+    ).data.get("team_id")
 
+    if team_id is None:  # means that user.team_id = NULL
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="user is not associated with any team",
+        )
+
+    async with file_lock:
+        uploads_dir.mkdir(exist_ok=True)
+
+    if file.filename is not None:
         # use team id, but for now just test with the uid
-        _, extension = os.path.splitext(file.filename or "test.cpp")
-        new_fname = user.id + extension
+        suffix = pathlib.Path(file.filename or "test.cpp").suffix
+        team_fname = pathlib.Path(str(team_id) + suffix)
 
-        # save the file to uploads directory
-        uploads_dir = os.path.abspath(os.path.join("..", "poker_tournament_uploads"))
-        os.makedirs(uploads_dir, exist_ok=True)
+        # joining paths
+        file_path = uploads_dir / team_fname
 
-        if file.filename is not None:
-            file_path = os.path.join(uploads_dir, new_fname)
-
-            with open(file_path, "w", encoding="utf-8") as f:
+        async with file_lock:
+            # save the file to uploads directory
+            with file_path.open("w", encoding="utf-8") as f:
                 f.write(content_str)
 
-            # get team id from foreign key in users table
-            team_id_res = (
-                db_client.table("users")
-                .select("team_id")
-                .eq("id", user.id)
-                .single()
-                .execute()
-            )
+        # set team.has_submitted_code to true
+        db_client.table("teams").update({"has_submitted_code": True}).eq(
+            "id", team_id
+        ).execute()
 
-            # set has_submitted_code to true
-            db_client.table("teams").update({"has_submitted_code": True}).eq(
-                "id", team_id_res.data.get("team_id")
-            ).execute()
+        return {"file_saved_as": team_fname}
 
-            return {"file_saved_as": new_fname}
-        else:
-            raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
-        # if file.filename.endswith(".py"):
-        #     process = subprocess.run(
-        #         ["python3", file.filename],
-        #         capture_output=True,
-        #         text=True,
-        #         check=True,  # ensure the command raises exception on failure
-        #         cwd=uploads_dir,  # change directory to uploads
-        #     )
-        #     return {
-        #         "status": "success",
-        #         "stdout": process.stdout,
-        #         "stderr": process.stderr,
-        #         "message": "Python file processed successfully.",
-        #     }
-        # elif file.filename.endswith(".cpp"):
-        #     fname_no_ext = file.filename[:-4]
-        #     exec_cmd = f"{fname_no_ext}.exe" if os.name == "nt" else f"./{fname_no_ext}"
+    # if file.filename.endswith(".py"):
+    #     process = subprocess.run(
+    #         ["python3", file.filename],
+    #         capture_output=True,
+    #         text=True,
+    #         check=True,  # ensure the command raises exception on failure
+    #         cwd=uploads_dir,  # change directory to uploads
+    #     )
+    #     return {
+    #         "status": "success",
+    #         "stdout": process.stdout,
+    #         "stderr": process.stderr,
+    #         "message": "Python file processed successfully.",
+    #     }
+    # elif file.filename.endswith(".cpp"):
+    #     fname_no_ext = file.filename[:-4]
+    #     exec_cmd = f"{fname_no_ext}.exe" if os.name == "nt" else f"./{fname_no_ext}"
 
-        #     process = subprocess.run(
-        #         f"c++ {file.filename} -o {fname_no_ext} && {exec_cmd}",
-        #         shell=True,  # run the command in the shell, not list of args
-        #         capture_output=True,
-        #         text=True,
-        #         check=True,  # ensure the command raises exception on failure
-        #         cwd=uploads_dir,  # change directory to uploads
-        #     )
+    #     process = subprocess.run(
+    #         f"c++ {file.filename} -o {fname_no_ext} && {exec_cmd}",
+    #         shell=True,  # run the command in the shell, not list of args
+    #         capture_output=True,
+    #         text=True,
+    #         check=True,  # ensure the command raises exception on failure
+    #         cwd=uploads_dir,  # change directory to uploads
+    #     )
 
-        #     return {
-        #         "status": "success",
-        #         "stdout": process.stdout,
-        #         "stderr": process.stderr,
-        #         "message": "C++ file processed successfully.",
-        #     }
-        # else:
-        #     return {
-        #         "status": "error",
-        #         "message": "Unsupported file type. Only .py and .cpp files are allowed.",
-        #     }
-
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
-        )
+    #     return {
+    #         "status": "success",
+    #         "stdout": process.stdout,
+    #         "stderr": process.stderr,
+    #         "message": "C++ file processed successfully.",
+    #     }
+    # else:
+    #     return {
+    #         "status": "error",
+    #         "message": "Unsupported file type. Only .py and .cpp files are allowed.",
+    #     }
--- src/user.py
+++ src/user.py
@@ -8,11 +8,11 @@ user_router = APIRouter(prefix="/user", tags=["user"])
 
 @user_router.get(
     "/",
-    response_model=dict,
+    response_model=User,
     responses=auth_required_responses,
 )
 def get_user(user: User = Depends(verify_user)):
-    return user.model_dump()
+    return user
 
 
 @user_router.get(
