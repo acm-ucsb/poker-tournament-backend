ee47411d1056b8fb6c80458a852c04a21e0ba910
Daniel Cui <solunian@gmail.com>
2025-08-27 11:33:24 -0700

    chore: format ken code

--- src/core/card.py
+++ src/core/card.py
@@ -25,27 +25,29 @@ RANK = {
     1: "2",
 }
 
+
 class Card(BaseModel):
     rank: int = Field(..., ge=1, le=13)
     suit: int = Field(..., ge=1, le=4)
-    
-class Deck():
+
+
+class Deck:
     def __init__(self):
         self.reset()
-    
+
     def reset(self) -> None:
         self.used_card: list[int] = [0] * 52
-        
+
     def deal_card(self) -> Card:
         if sum(self.used_card) == 52:
             raise Exception("No more card left to deal")
-        
+
         card = None
         while card == None or self.used_card[card] == 1:
             rank = randint(1, 13)
             suit = randint(1, 4)
             card = suit * 4 + rank
-        
+
         self.used_card[card] = 1
-            
+
         return Card(rank=card[0], suit=card[1])
--- src/core/player.py
+++ src/core/player.py
@@ -4,6 +4,7 @@ from typing import Optional
 from pydantic import BaseModel
 from src.core.card import Card
 
+
 class ActionType(Enum):
     FOLD = 0
     CHECK = 1
@@ -11,36 +12,37 @@ class ActionType(Enum):
     BET = 3
     RAISE = 4
 
+
 class Action(BaseModel):
     action: ActionType
     amount: Optional[int] = None
+
 
 class Player:
     def __init__(self, player_id: str, starting_chips: int = 0):
         self.id: str = player_id
         self.chips: int = starting_chips
-        
+
         self.hand: list[Card] = []
-        
+
         self.is_all_in: bool = False
         self.has_folded: bool = False
-        
-        self.contribution: int = 0          # chips in current betting round
-        self.total_contributed: int = 0     # chips contributed in whole hand
-        
+
+        self.contribution: int = 0  # chips in current betting round
+        self.total_contributed: int = 0  # chips contributed in whole hand
+
     @property
     def is_eliminated(self):
         return self.chips <= 0
-    
+
     def new_hand(self):
         self.hand = []
         self.is_all_in = False
         self.has_folded = False
         self.contribution = 0
-    
+
     def new_round(self):
         self.new_hand()
         self.total_contributed = 0
-        
-    def act(self) -> Action:
-        ...
+
+    def act(self) -> Action: ...
