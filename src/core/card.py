2a51de6d72dc790170222b40fb422d09790e8d34
Ken Thampiratwong <ken.thampiratwong@gmail.com>
2025-08-30 23:56:45 +0700

    fix: account for tie-break during hand eval

--- src/core/card.py
+++ src/core/card.py
@@ -2,31 +2,31 @@ from random import randint
 from pydantic import BaseModel, Field
 
 SUIT = {
-    1: "S",
-    2: "H",
-    3: "D",
-    4: "C",
+    0: "S",
+    1: "H",
+    2: "D",
+    3: "C",
 }
 
 RANK = {
-    13: "A",
-    12: "K",
-    11: "Q",
-    10: "J",
-    9: "10",
-    8: "9",
-    7: "8",
-    6: "7",
-    5: "6",
-    4: "5",
-    3: "4",
-    2: "3",
-    1: "2",
+    12: "A",
+    11: "K",
+    10: "Q",
+    9: "J",
+    8: "10",
+    7: "9",
+    6: "8",
+    5: "7",
+    4: "6",
+    3: "5",
+    2: "4",
+    1: "3",
+    0: "2",
 }
 
 class Card(BaseModel):
-    rank: int = Field(..., ge=1, le=13)
-    suit: int = Field(..., ge=1, le=4)
+    rank: int = Field(..., ge=0, le=12)
+    suit: int = Field(..., ge=0, le=3)
     
 class Deck():
     def __init__(self):
@@ -48,237 +48,3 @@ class Deck():
         self.used_card[card] = 1
             
         return Card(rank=card[0], suit=card[1])
-    
-    #TODO: test all of these functions
-    # I ran all of these functions through gemini and it said they were correct
-
-
-    """
-    precondition: called from the deck object in the table class
-    precondition: cards is a list of Card objects
-    precondition: len(cards) == 7
-    precondition: cards contains no duplicate cards
-    precondition: cards contains only valid cards (1 <= rank <= 13, 1 <= suit <= 4)
-    """
-
-    # determine the strongest hand from a list of cards
-    @classmethod
-    def hand_strength(cls, cards: list[Card]) -> int:
-        """
-        check all strengths in order from strongest to weakest
-        an integer representing the highest rank in each hand is returned
-        return strength value + rank of highest card in the hand
-        """
-        
-        # straight flush
-        check = cls._check_straight_flush(cards)
-        if check != -1:
-            return 800 + check # highest strength
-        
-        # four of a kind
-        check = cls._check_four_of_a_kind(cards)
-        if check != -1:
-            return 700 + check
-        
-        # full house
-        check = cls._check_full_house(cards)
-        if check != -1:
-            return 600 + check  
-        
-        # flush
-        check = cls._check_flush(cards)
-        if check != -1:
-            return 500 + check
-        
-        # straight
-        check = cls._check_straight(cards)
-        if check != -1:
-            return 400 + check
-        
-        # three of a kind
-        check = cls._check_three_of_a_kind(cards)
-        if check != -1:
-            return 300 + check
-        
-        # two pair
-        check = cls._check_two_pair(cards)
-        if check != -1:
-            return 200 + check
-        
-        # one pair
-        check = cls._check_one_pair(cards)
-        if check != -1:
-            return 100 + check  
-        
-        # high card
-        check = cls._check_high_card(cards)
-        return check # lowest strength
-        
-
-    """
-    private helper methods for hand strength
-    call in order of strongest to weakest hand
-    """
-
-    # return rank of highest card in the straight flush, -1 if no straight flush found
-    @classmethod
-    def _check_straight_flush(cls, cards: list[Card]) -> int:
-        suits = {}
-        for card in cards:
-            if card.suit in suits:
-                suits[card.suit] += 1
-            else:
-                suits[card.suit] = 1
-
-        # check flush
-        if all(v < 5 for v in suits.values()):
-            return -1 # no flush found
-        
-        desired_suit = [suit for suit, v in suits.items() if v >= 5][0]
-        cards_in_suit = [card for card in cards if card.suit == desired_suit]
-        return cls._check_straight(cards_in_suit)
-
-    # return rank of 4 of a kind, -1 if no four found
-    @staticmethod
-    def _check_four_of_a_kind(cards: list[Card]) -> int:
-        vals = {}
-        for card in cards:
-            if card.rank in vals:
-                vals[card.rank] += 1
-            else:
-                vals[card.rank] = 1
-
-        vals = sorted(vals.items(), key=lambda x: x[1], reverse=True)
-        # if a 4 of a kind exists, it will be the first element
-        if vals[0][1] == 4:
-            return vals[0][0]
-        return -1
-
-    # return rank of highest card in the full house, -1 if no full house found
-    @staticmethod
-    def _check_full_house(cards: list[Card]) -> int:
-        vals = {}
-        for card in cards:
-            if card.rank in vals:
-                vals[card.rank] += 1
-            else:
-                vals[card.rank] = 1
-
-        three_of_a_kind = -1
-        pair = -1
-        # sort descending by frequency
-        vals = sorted(vals.items(), key=lambda x: x[1], reverse=True) 
-        for rank, v in vals:
-            # highest three of a kind
-            if v >= 3 and three_of_a_kind == -1:
-                three_of_a_kind = rank
-            # next highest pair
-            elif v >= 2 and pair == -1:
-                pair = rank
-            
-            if three_of_a_kind != -1 and pair != -1:
-                return three_of_a_kind
-            
-        return -1
-
-    # return rank of highest card in the flush, -1 if no flush found
-    @staticmethod
-    def _check_flush(cards: list[Card]) -> int:
-        suits = {}
-        for card in cards:
-            if card.suit in suits:
-                suits[card.suit] += 1
-            else:
-                suits[card.suit] = 1
-
-        # no flush found
-        found = all(v < 5 for v in suits.values())
-        if found: # all values < 5
-            return -1
-        
-        #return highest card in the flush
-        desired_suit = [suit for suit, v in suits.items() if v >= 5][0]
-        max_rank = max([card.rank for card in cards if card.suit == desired_suit])
-        return max_rank
-
-    # return rank of highest card in the straight, -1 if no straight found
-    @staticmethod
-    def _check_straight(cards: list[Card]) -> int:
-        vals = set()
-        for card in cards:
-            vals.add(card.rank)
-        # ace can be high or lowe
-        if 13 in vals:
-            vals.add(0)
-        vals = sorted(list(vals))
-        # consecutive differences
-        diffs = [vals[i+1] - vals[i] for i in range(len(vals)-1)]
-
-        # look for 4 consecutive 1s in diffs
-        # start from the end
-        for i in reversed(range(len(diffs) - 3)):
-            if diffs[i:i+4] == [1, 1, 1, 1]:
-                return vals[i+4] # highest card in the straight
-            
-        return -1
-
-    #return rank of the three, -1 if no three found
-    @staticmethod
-    def _check_three_of_a_kind(cards: list[Card]) -> int:
-        vals = {}
-        for card in cards:
-            if card.rank in vals:
-                vals[card.rank] += 1
-            else:
-                vals[card.rank] = 1
-
-        #sort bu rank
-        vals = sorted(vals.items(), key = lambda x: x[0], reverse=True)
-        # highest three of a kind
-        for rank, v in vals:
-            if v == 3:
-                return rank
-            
-        return -1
-
-    # returns rank of highest pair, -1 if no pair found
-    @staticmethod
-    def _check_two_pair(cards: list[Card]) -> int:
-        vals = {}
-        for card in cards:
-            if card.rank in vals:
-                vals[card.rank] += 1
-            else:
-                vals[card.rank] = 1
-
-        vals = sorted(vals.items(), key = lambda x: x[0], reverse=True)
-        pairs = []
-        for rank, v in vals:
-            if v == 2:
-                pairs.append(rank)
-                if len(pairs) == 2:
-                    return max(pairs)
-            
-        return -1 
-
-    # only call this method if all previous methods return false
-    @staticmethod
-    def _check_one_pair(cards: list[Card]) -> int:
-        vals = {}
-        for card in cards:
-            if card.rank in vals:
-                vals[card.rank] += 1
-            else:
-                vals[card.rank] = 1
-        # sort by rank
-        vals = sorted(vals.items(), key = lambda x: x[0], reverse=True)
-        # return highest pair
-        for rank, v in vals:
-            if v == 2:
-                return rank
-            
-        return -1 # no pair found
-    
-    @staticmethod
-    def _check_high_card(cards: list[Card]) -> int:
-        return max([card.rank for card in cards])
--- src/core/player.py
+++ src/core/player.py
@@ -72,3 +72,223 @@ class Player:
         self.chips -= amount
         self.contribution += amount
         return amount
+    
+    def build_best_hand(self, community_cards: list[Card]) -> tuple[int, list[Card]]:
+        hand = self.hand + community_cards
+        
+        build_order = [
+            self._build_straight_flush,
+            self._build_four_of_a_kind,
+            self._build_full_house,
+            self._build_flush,
+            self._build_straight,
+            self._build_three_of_a_kind,
+            self._build_two_pair,
+            self._build_one_pair,
+            self._build_high_card,
+        ]
+        
+        
+        # TODO: (fix) make sure that the best_hand is sorted by eval types
+        for idx, build in enumerate(build_order):
+            best_hand = build(hand)
+            if len(best_hand) == 5:
+                hand_rank = len(build_order) - idx
+                return (hand_rank, best_hand)
+    
+    #TODO: test
+    @staticmethod
+    def _build_straight_flush(cards: list[Card]) -> list[Card]:
+        suits = [0] * 4
+        for card in cards:
+            suits[card.suit] += 1
+            
+        best_suit = -1
+        for suit, count in enumerate(suits):
+            if count >= 5:
+                best_suit = suit
+                break
+            
+        if best_suit == -1:
+            return []
+        
+        return Player._build_straight([card for card in cards if card.suit == best_suit])
+    
+    #TODO: test
+    @staticmethod
+    def _build_four_of_a_kind(cards: list[Card]) -> list[Card]:
+        ranks = [0] * 13
+        
+        for card in cards:
+            ranks[card.rank - 1] += 1
+        
+        best_rank = -1
+        for rank, count in reversed(enumerate(ranks)):
+            if count == 4:
+                best_rank = rank
+                break
+            
+        if best_rank == -1:
+            return []
+        
+        best_hand = [card for card in cards if card.rank == best_rank]
+        best_hand.extend(Player._build_high_card([card for card in cards if card.rank != best_rank]))[0]
+        
+        return best_hand
+    
+    # TODO: test
+    @staticmethod
+    def _build_full_house(cards: list[Card]) -> list[Card]:
+        ranks = [0] * 13
+        for card in cards:
+            ranks[card.rank - 1] += 1
+
+        three_of_a_kind_rank = -1
+        pair_rank = -1
+        for rank, count in reversed(enumerate(ranks)):
+            if count >= 3 and three_of_a_kind_rank == -1:
+                three_of_a_kind_rank = rank
+            elif pair_rank >= 2 and pair_rank == -1:
+                pair_rank = rank
+        
+        if three_of_a_kind_rank != -1 and pair_rank != -1:
+            return []
+        
+        return [card for card in cards if card.rank == three_of_a_kind_rank][:3] + [card for card in cards if card.rank == pair_rank][:2]
+    
+    # TODO: test
+    @staticmethod
+    def _build_flush(cards: list[Card]) -> list[Card]:
+        suits = [0] * 4
+        
+        for card in cards:
+            suits[card.suit] += 1
+
+        best_suit = -1
+        for suit, count in enumerate(suits):
+            if count >= 5:
+                best_suit = suit
+                break
+            
+        if best_suit == -1:
+            return []
+        
+        best_hand = [card for card in cards if card.suit == best_suit]
+        best_hand.sort(key=lambda card: card.rank, reverse=True)
+        
+        return best_hand[:5]
+    
+    # TODO: test
+    @staticmethod
+    def _build_straight(cards: list[Card]) -> list[Card]:
+        # 14 to account for the ace low
+        ranks = [0] * 14
+        for card in cards:
+            ranks[card.rank + 1] += 1
+            if card.rank == 12:
+                ranks[0] += 1
+        
+        best_high = -1
+        current_length = 0
+        for rank, count in enumerate(ranks):
+            if count != 0:
+                current_length += 1
+                
+                if current_length >= 5:
+                    best_high = rank
+            else:
+                current_length = 0
+                
+        if best_high == -1:
+            return []
+        
+        cards_sorted = sorted(cards, key=lambda card: card.rank)
+        
+        # +1 to offset rank shifted due to low ace
+        current_rank = best_high - 5 + 1
+        best_hand = []
+        
+        if current_rank == 0:
+            current_rank = 1
+            best_hand.append(cards_sorted[-1])
+            
+        for card in cards_sorted:
+            if len(best_hand) == 5:
+                break
+            
+            if card.rank == current_rank:
+                best_hand.append(card)
+                current_rank += 1
+        
+        return best_hand
+    
+    #TODO: test
+    @staticmethod
+    def _build_three_of_a_kind(cards: list[Card]) -> list[Card]:
+        ranks = [0] * 13
+        for card in cards:
+            ranks[card.rank] += 1
+        
+        best_rank = -1
+        for rank, count in reversed(enumerate(ranks)):
+            if count == 3:
+                best_rank = rank
+                
+        if best_rank == -1:
+            return []
+                
+        best_hand = [card for card in cards if card.rank == best_rank]
+        best_hand.extend(Player._build_high_card([card for card in cards if card.rank != best_rank])[:2])
+            
+        return best_hand
+
+    #TODO: test
+    @staticmethod
+    def _build_two_pair(cards: list[Card]) -> list[Card]:
+        ranks = [0] * 13
+        for card in cards:
+            ranks[card.rank] += 1
+        
+        best_rank_1, best_rank_2 = -1, -1
+        for rank, count in reversed(enumerate(ranks)):
+            if count == 2:
+                if best_rank_1 == -1:
+                    best_rank_1 = rank
+                elif best_rank_2 == -1:
+                    best_rank_2 = rank
+                else:
+                    break
+                
+        if best_rank_1 == -1 or best_rank_2 == -1:
+            return []
+        
+        best_hand = [card for card in cards if card.rank == best_rank_1 or card.rank == best_rank_2]
+        best_hand.extend(Player._build_high_card([card for card in cards if card.rank != best_rank_1 and card.rank != best_rank_2])[0])
+            
+        return best_hand
+    
+    #TODO: test
+    @staticmethod
+    def _build_one_pair(cards: list[Card]) -> list[Card]:
+        ranks = [0] * 13
+        for card in cards:
+            ranks[card.rank] += 1
+            
+        best_rank = -1
+        for rank, count in reversed(enumerate(ranks)):
+            if count == 2:
+                best_rank = rank
+                break
+            
+        if best_rank == -1:
+            return []
+            
+        best_hand = [card for card in cards if card.rank == best_rank]
+        best_hand.extend(Player._build_high_card([card for card in cards if card.rank != best_rank])[:3])
+        
+        return best_hand
+    
+    #TODO: test
+    @staticmethod
+    def _build_high_card(cards: list[Card]) -> list[Card]:
+        return sorted(cards, key=lambda card: card.rank, reverse=True)[:5]
