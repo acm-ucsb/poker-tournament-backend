215e0e1df1a41d09d9d5292f775f99605e907851
Daniel Cui <solunian@gmail.com>
2025-11-14 15:41:08 -0800

    retable, no table deletion yet

--- src/core/tournament.py
+++ src/core/tournament.py
@@ -5,7 +5,7 @@ import math
 
 import traceback
 
-MAX_TABLE_SIZE = 9
+MAX_TABLE_SIZE = 8
 DEFAULT_TOURNAMENT_ID = "f6fd507b-42fb-4fba-a0d3-e9ded05aeca5"
 
 BLIND_INCREASE = 2
@@ -117,8 +117,9 @@ class Tournament:
     ):
         # table_ids to specify which tables to make moves on, default None for make_move on all
         # moves is for human moves so must be same len as table_ids, default None for no human moves
-        result_strs = []
 
+        # actual running files!
+        result_strs = []
         for table in self.tables.values():
             try:
                 result_strs.append(await table.make_move())
@@ -127,6 +128,105 @@ class Tournament:
                 result_strs.append(
                     f"did not run, {e}, {e.args}, {e.with_traceback(None)}, {traceback.format_exc()}"
                 )
+
+        # table reduction!
+        num_total_tables = len(self.tables)
+        num_total_teams = 0
+        for table in self.tables.values():
+            num_total_teams += len(table.state.players)
+
+        if num_total_teams <= (num_total_tables - 1) * MAX_TABLE_SIZE:
+            pass
+
+        # ============ #
+        # RETABLING!!! #
+        # ============ #
+
+        while True:
+            sorted_tables = sorted(
+                self.tables.values(), key=lambda x: len(x.state.players)
+            )
+
+            min_table = sorted_tables[0]
+            max_table = sorted_tables[-1]
+
+            if (
+                min_table.table_id == max_table.table_id
+                or len(max_table.state.players) - len(min_table.state.players) < 2
+            ):
+                break  # already balanced
+
+            # taken from closest after bb
+            team_to_move_index = (max_table.state.index_of_small_blind + 2) % len(
+                max_table.state.players
+            )
+
+            run_count = 0
+            while (
+                team_to_move_index == max_table.state.small_blind
+                or team_to_move_index
+                == (max_table.state.small_blind + 1) % len(max_table.state.players)
+                or team_to_move_index == max_table.state.index_to_action
+            ):
+                if run_count > len(max_table.state.players):
+                    break
+                team_to_move_index = (team_to_move_index + 1) % len(
+                    max_table.state.players
+                )
+                run_count += 1
+            if run_count > len(max_table.state.players):
+                break
+
+            team_id = max_table.state.players.pop(team_to_move_index)
+            max_table.state.players_cards.pop(team_to_move_index)
+            held_money = max_table.state.held_money.pop(team_to_move_index)
+            bet_money = max_table.state.bet_money.pop(team_to_move_index)
+
+            pot_value_without_current_round = max_table.state.pots[0].value
+            for player_bet in max_table.state.bet_money:
+                pot_value_without_current_round -= player_bet
+
+            divided_main_pot_value = pot_value_without_current_round // len(
+                max_table.state.pots[0].players
+            )
+            held_money += divided_main_pot_value + bet_money
+            max_table.state.pots[0].value -= divided_main_pot_value + bet_money
+            max_table.state.pots[0].players.remove(team_id)
+
+            # case of if any sidepots exist
+            for pot in max_table.state.pots[1:]:
+                if team_id in pot.players:
+                    divided_pot_value = pot.value // len(pot.players)
+                    held_money += divided_pot_value
+                    pot.value -= divided_pot_value
+                    pot.players.remove(team_id)
+
+            if team_to_move_index < max_table.state.index_of_small_blind:
+                max_table.state.index_of_small_blind -= 1
+            if team_to_move_index < max_table.state.index_to_action:
+                max_table.state.index_to_action -= 1
+
+            # moved to closest before sb (button or worse)
+            index_before_sb = (
+                min_table.state.index_of_small_blind - 1 + len(min_table.state.players)
+            ) % len(min_table.state.players)
+
+            min_table.state.players.insert(index_before_sb, team_id)
+            min_table.state.players_cards.insert(index_before_sb, [])
+            min_table.state.held_money.insert(index_before_sb, held_money)
+            min_table.state.bet_money.insert(index_before_sb, -1)
+
+            if index_before_sb <= min_table.state.index_of_small_blind:
+                min_table.state.index_of_small_blind += 1
+            if index_before_sb <= min_table.state.index_to_action:
+                min_table.state.index_to_action += 1
+
+            db_client.table("teams").update({"table_id": min_table.table_id}).eq(
+                "id", team_id
+            ).execute()
+
+            Table.write_state_to_db(max_table.table_id, max_table.state)
+            Table.write_state_to_db(min_table.table_id, min_table.state)
 
         return result_strs
 
